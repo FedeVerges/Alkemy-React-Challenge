1cebabb0c335c0ad55b6cc4f8c0f00c77d0d99d9
Fede Verges <fede_16_98@hotmail.com>
2021-09-22 23:16:40 -0300

    Login done

--- heroes-app/src/App.js
+++ heroes-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React from 'react';
-import {Form} from './componentes/Form.js';
+import SignupForm from './componentes/Form.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form/>
+        <SignupForm/>
       </header>
     </div>
   );
--- heroes-app/src/componentes/Form.js
+++ heroes-app/src/componentes/Form.js
@@ -1,26 +1,79 @@
-import React, {
-    Component
-} from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
 
-class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            valid: false,
-            response: '',
-        };
+const validateFields = values => {
+    const errors = {};
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
     }
+    if (!values.password) {
+        errors.password = 'Required';
+    } else if (values.password.length < 8) {
+        errors.password = 'To Short Password';
+    }
+    // Agregar control de comillas y de inyeccion sql. 
+    return errors;
+};
 
-    render() {
-        return (
-            <form class="email-form" method="post">
-        <input type="email" name="email" title="Escribe un email valido como por ejemplo: hola@soyUnSuperheroe.com" pattern="^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$" placeholder="Escribe tu email" required/>        <input type="password" name="password" placeholder="Escribe tu password" minlength="8"
-            title="Escribe password con mas de 8 caracteres" required/>
-        <input type="submit" value="Enviar"/>
-                <div class="email-form-response none"></div>
+const SignupForm = () => {
+    // Pass the useFormik() hook initial form values and a submit function that will
+    // be called when the form is submitted
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validate: validateFields,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+            // post con email y password. Despues insertar jwt y renderizar la barra.
+
+        },
+    });
+    return (
+        <div className="login-box">
+            <h2>Login</h2>
+            <form className="email-form" onSubmit={formik.handleSubmit}>
+                <div className="user-box">
+                    <label>Email</label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        className="email-form"
+                        placeholder="Ingresa tu email"
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        required
+                    />
+                    {formik.touched.email && formik.errors.email ? <div className="email-form-error">{formik.errors.email}</div> : null}
+                </div>
+                <div className="user-box">
+                    <label>Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        className="email-form"
+                        placeholder="Ingresa tu password"
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        required
+                    />
+                    {formik.touched.password && formik.errors.password ? <div className="email-form-error">{formik.errors.password}</div> : null}
+                </div>
+                <input type="submit" value="Enviar"></input>
+                <div className="email-form-response">
+                    <p>
+                    </p>
+                </div>
             </form>
-            );
-    }
-}
+        </div>
+    );
+};
 
-export {Form};
+export default SignupForm;
